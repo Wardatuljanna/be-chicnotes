a3837b5ed87ef50fda3bc1b043b284ec2a1f81ee
wardatuljannah13 <147989656+wardatuljannah13@users.noreply.github.com>
2024-12-14 13:37:02 +0700

    Update note.js

--- models/note.js
+++ models/note.js
@@ -11,7 +11,11 @@ const Note = sequelize.define('Note', {
     type: DataTypes.TEXT,
     allowNull: false
   }
+}, {
+  tableName: 'notes' // Menentukan nama tabel yang digunakan di database
 });
 
+// Menambahkan relasi ke User
 Note.belongsTo(User, { foreignKey: 'userId' });
+
 module.exports = Note;
